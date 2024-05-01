90d735a2b9eb0870b2a6612d8cadbad2f27dfd64
Herbert <117818214+HvdW144@users.noreply.github.com>
2024-05-01 16:42:58 +0200

    updated sudokuToStringArray tests

--- test/util/sudokuStringToStringArray.util.test.ts
+++ test/util/sudokuStringToStringArray.util.test.ts
@@ -2,6 +2,7 @@ import { sudokuStringToStringArray } from "../../src/util/sudokuStringToStringAr
 
 describe("stringToStringArray", () => {
   it("should convert a valid Sudoku string of 81 characters (grid size 9) to an array", () => {
+    // arrange
     const sudokuString =
       ".6.27.3..............5.8.9..7.1..........32...5.769...6........1...92.3..358.1..2";
     const gridSize = 9;
@@ -18,16 +19,38 @@ describe("stringToStringArray", () => {
       [".", "3", "5", "8", ".", "1", ".", ".", "2"],
     ];
 
+    // act
     const result = sudokuStringToStringArray(sudokuString, gridSize);
+
+    // assert
     expect(result).toEqual(expectedArray);
   });
 
   it("should throw an error for invalid string length", () => {
+    // arrange
     const sudokuString = "...";
     const gridSize = 2;
 
+    // act and assert
     expect(() => sudokuStringToStringArray(sudokuString, gridSize)).toThrow(
       "Invalid string length for given grid size."
     );
+  });
+
+  it("should convert a valid Sudoku string of 4 characters (grid size 2) to an array", () => {
+    // arrange
+    const sudokuString = ".6.2";
+    const gridSize = 2;
+
+    const expectedArray = [
+      [".", "6"],
+      [".", "2"],
+    ];
+
+    // act
+    const result = sudokuStringToStringArray(sudokuString, gridSize);
+
+    // assert
+    expect(result).toEqual(expectedArray);
   });
 });
