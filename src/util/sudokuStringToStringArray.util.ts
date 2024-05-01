ce73bca6ddea7955cafdda0ac652866dc0002462
Herbert <117818214+HvdW144@users.noreply.github.com>
2024-05-01 16:39:55 +0200

    fixed sudokuStringToStringArray

--- src/util/sudokuStringToStringArray.util.ts
+++ src/util/sudokuStringToStringArray.util.ts
@@ -1,15 +1,17 @@
 export function sudokuStringToStringArray(
   sudokuString: string,
   gridSize: number
-): string[] {
+): string[][] {
   if (sudokuString.length !== gridSize ** 2) {
     throw new Error("Invalid string length for given grid size.");
   }
 
-  const sudokuArray: string[] = [];
-  for (let i = 0; i < gridSize; i++) {
-    const row = sudokuString.slice(i * gridSize, i * gridSize + gridSize);
-    sudokuArray.push(row);
+  const Array1D = Array.from(sudokuString);
+
+  const Array2D: string[][] = [];
+  for (let i = 0; i < 9; i++) {
+    Array2D.push(Array1D.slice(i * gridSize, (i + 1) * gridSize));
   }
-  return sudokuArray;
+
+  return Array2D;
 }
