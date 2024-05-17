ecf4d450cc0e2e21c2db92d04085f6641e171f38
Herbert <117818214+HvdW144@users.noreply.github.com>
2024-05-17 19:37:16 +0200

    Create boxValidator.class.test.ts
    not working tho

---
+++ test/validators/box-validator/boxValidator.class.test.ts
@@ -0,0 +1,57 @@
+import { BoxValidator } from "../../../src/validators/box-validator/boxValidator.class";
+
+describe("BoxValidator", () => {
+  it("validate - should return empty array for a valid 4x4 grid", () => {
+    // arrange
+    const boxValidator = new BoxValidator();
+    const sudokuString = "1234341223414123";
+
+    // act
+    const result = boxValidator.validate(sudokuString, 4);
+
+    // assert
+    expect(result.isValid).toBe(true);
+    expect(result.duplicates).toStrictEqual([]);
+  });
+
+  it("validate - should return array with duplicates for an invalid 4x4 grid", () => {
+    // arrange
+    const boxValidator = new BoxValidator();
+    const sudokuString = "1234341223414124";
+
+    // act
+    const result = boxValidator.validate(sudokuString, 4);
+
+    // assert
+    expect(result.isValid).toBe(false);
+    expect(result.duplicates).toStrictEqual([10, 15]);
+  });
+
+  it("validate - should return empty array for a valid 9x9 grid", () => {
+    // arrange
+    const boxValidator = new BoxValidator();
+    const sudokuString =
+      "123456789234567891345678912456789123567891234678912345789123456891234567912345678";
+
+    // act
+    const result = boxValidator.validate(sudokuString, 9);
+
+    // assert
+    expect(result.isValid).toBe(true);
+    expect(result.duplicates).toStrictEqual([]);
+  });
+
+  it("validate - should return array with duplicates for an invalid 9x9 grid", () => {
+    // arrange
+    const boxValidator = new BoxValidator();
+    const sudokuString =
+      "123456789234567891345678913456789123567891234678912345789123456891234567912345677";
+
+    // act
+    const result = boxValidator.validate(sudokuString, 9);
+
+    // assert
+    expect(result.isValid).toBe(false);
+    expect(result.duplicates).toStrictEqual([18, 26, 79, 80]);
+  });
+});
