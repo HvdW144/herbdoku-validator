42de23a501f0cff561ab073824aeeb14c0a5477a
Herbert <117818214+HvdW144@users.noreply.github.com>
2024-05-25 15:25:51 +0200

    added one test to herbdoku.test

--- test/herbdoku.test.ts
+++ test/herbdoku.test.ts
@@ -1,5 +1,21 @@
-describe("EMPTY", () => {
-  it("EMPTY", () => {
-    expect(true).toBe(true); // Simple assertion that always passes
+import Herbdoku from "../src/herbdoku";
+
+describe("Herbdoku", () => {
+  let herbdoku: Herbdoku;
+
+  it("validateDefault - should call validateRows, validateColumns, and validateBoxes", () => {
+    // arrange
+    herbdoku = new Herbdoku("1234341221434321", 4);
+    const validateRowsMock = jest.spyOn(herbdoku, "validateRows");
+    const validateColumnsMock = jest.spyOn(herbdoku, "validateColumns");
+    const validateBoxesMock = jest.spyOn(herbdoku, "validateBoxes");
+
+    // act
+    herbdoku.validateDefault();
+
+    // assert
+    expect(validateRowsMock).toHaveBeenCalled();
+    expect(validateColumnsMock).toHaveBeenCalled();
+    expect(validateBoxesMock).toHaveBeenCalled();
   });
 });
