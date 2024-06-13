399996ba2444ce64fbcc80e56a15b613ac6dd471
Herbert <117818214+HvdW144@users.noreply.github.com>
2024-06-13 16:32:36 +0200

    Update whiteKropkiValidator.class.ts

--- src/validators/kropki-validators/white-kropki-validator/whiteKropkiValidator.class.ts
+++ src/validators/kropki-validators/white-kropki-validator/whiteKropkiValidator.class.ts
@@ -2,29 +2,30 @@ import { Validator } from "../../validator.abstract";
 import type { ValidatorResult } from "../../validatorResult.interface";
 
 export class WhiteKropkiValidator extends Validator {
-  private whiteKropkiArray: number[][];
+  private whiteKropkiArray: number[][]; //TODO: or number[]
 
   constructor(whiteKropkiArray: number[][]) {
     super();
     this.whiteKropkiArray = whiteKropkiArray;
   }
 
-  public validate(sudokuString: string[][], gridSize: number): ValidatorResult {
+  public validate(sudokuString: string, gridSize: number): ValidatorResult {
     if (sudokuString.length !== gridSize ** 2) {
       throw new Error("Invalid grid size for given string size.");
     }
 
     this.whiteKropkiArray.forEach((KropkiDot) => {
-      this.validateKropkiDot(KropkiDot, gridSize);
+      this.validateKropkiDot(sudokuString, gridSize, KropkiDot);
     });
 
     return { isValid: false, invalidIndexes: [] };
   }
 
-  //TODO: make public maybe
+  //TODO: make public maybe (dont, use whiteKropkiArray as number[] instead)
   private validateKropkiDot(
-    kropkiDotArray: number[],
-    gridSize: number
+    sudokuString: string,
+    gridSize: number,
+    kropkiDotArray: number[]
   ): ValidatorResult {
     if (kropkiDotArray.length !== 3 || 4) {
       throw new Error("Kropki dot array length should be 3 or 4.");
@@ -32,10 +33,19 @@ export class WhiteKropkiValidator extends Validator {
 
     const [x1 = -1, x2 = -1, value = 1] = kropkiDotArray;
 
-    if (x1 === -1 || x2 === -1) {
-      throw new Error("Kropki dot array length should be 2 or 3.");
+    const value1 = sudokuString.charAt(x1); //it's a string..
+    const value2 = sudokuString.charAt(x2);
+
+    if (x1 < 0 || x1 >= gridSize ** 2 || x2 < 0 || x2 >= gridSize ** 2) {
+      throw new Error("The indexes of the kropki dot are out of bounds.");
     }
 
-    return { isValid: false, invalidIndexes: [] };
+    if (x1 - value == x2 || x2 - value == x1) {
+      return { isValid: true, invalidIndexes: [] };
+    } else {
+      return { isValid: false, invalidIndexes: [] };
+    }
+
+    return { isValid: false, invalidIndexes: [] }; //TODO: implement
   }
 }
