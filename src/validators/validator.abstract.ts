4d1c6be1db9df7589dbb6e8a316ab0bbcc8c6a71
Herbert <117818214+HvdW144@users.noreply.github.com>
2024-06-14 15:26:51 +0200

    Update validator.abstract.ts

--- src/validators/validator.abstract.ts
+++ src/validators/validator.abstract.ts
@@ -1,6 +1,7 @@
 import type { ValidatorResult } from "./validatorResult.interface";
 
 export abstract class Validator {
+  //TODO: make interface
   public abstract validate(
     sudokuString2D: string | string[][],
     gridSize: number
