4ebc94c8d55618a044417cccec098f0e75adb2b3
Herbert <117818214+HvdW144@users.noreply.github.com>
2024-06-18 15:19:50 +0200

    created one validator only

--- src/validators/kropki-validator/kropkiValidator.class.ts
+++ src/validators/kropki-validator/kropkiValidator.class.ts
@@ -1,8 +1,6 @@
 import { Validator } from "../validator.abstract";
 import type { ValidatorResult } from "../validatorResult.interface";
-import { BlackKropkiValidator } from "./black-kropki-validator/blackKropkiValidator.class";
 import type { KropkiDot } from "./kropkiDot.interface";
-import { WhiteKropkiValidator } from "./white-kropki-validator/whiteKropkiValidator.class";
 
 export class KropkiValidator extends Validator {
   private kropkiArray: KropkiDot[];
@@ -13,37 +11,90 @@ export class KropkiValidator extends Validator {
   }
 
   public validate(sudokuString: string, gridSize: number): ValidatorResult {
-    const whiteKropkiDots = this.kropkiArray.filter((dot) => {
-      dot.kropkiType === "white" || dot.kropkiType === 0;
-    });
-    const blackKropkiDots = this.kropkiArray.filter((dot) => {
-      dot.kropkiType === "black" || dot.kropkiType === 1;
-    });
-
     let finalResult: ValidatorResult = {
       isValid: true,
       messages: [],
       invalidIndexes: [],
     };
 
-    if (whiteKropkiDots.length !== 0) {
-      const whiteKropkiValidator = new WhiteKropkiValidator(whiteKropkiDots);
-      const result = whiteKropkiValidator.validate(sudokuString, gridSize);
-      if (!result.isValid) {
-        finalResult.isValid = false;
-        if (!finalResult.invalidIndexes) {
-          finalResult.invalidIndexes = [];
+    this.kropkiArray.forEach((kropkiDot) => {
+      let result: ValidatorResult;
+      if (
+        !(
+          kropkiDot.x1 < 0 ||
+          kropkiDot.x1 >= gridSize ** 2 ||
+          kropkiDot.x2 < 0 ||
+          kropkiDot.x2 >= gridSize ** 2
+        )
+      ) {
+        if (kropkiDot.kropkiType === "white" || kropkiDot.kropkiType === 0) {
+          result = this.validateWhiteKropkiDot(sudokuString, kropkiDot);
+        } else if (
+          kropkiDot.kropkiType === "black" ||
+          kropkiDot.kropkiType === 1
+        ) {
+          result = this.validateBlackKropkiDot(sudokuString, kropkiDot);
+        } else {
+          throw new Error("Invalid kropki type.");
         }
-        finalResult.invalidIndexes.push(...(result.invalidIndexes || []));
-      }
-    }
 
-    const blackKropkiValidator = new BlackKropkiValidator();
+        if (!result.isValid) {
+          finalResult.isValid = false;
+          finalResult.invalidIndexes.push(...result.invalidIndexes);
+        }
+      } else {
+        result = {
+          isValid: true,
+          messages: [
+            `One or more indexes of the kropki dot with indexes ${kropkiDot.x1} and ${kropkiDot.x2} are out of bounds. Result is ignored`,
+          ],
+          invalidIndexes: [],
+        };
+      }
+    });
+
+    return finalResult;
+  }
+
+  private validateWhiteKropkiDot(
+    sudokuString: string,
+    whiteKropkiDot: KropkiDot
+  ): ValidatorResult {
+    const value1 = Number(sudokuString.charAt(whiteKropkiDot.x1));
+    const value2 = Number(sudokuString.charAt(whiteKropkiDot.x2));
+
+    if (
+      value1 - (whiteKropkiDot.kropkiValue || 1) === value2 ||
+      value2 - (whiteKropkiDot.kropkiValue || 1) === value1
+    ) {
+      return { isValid: true, messages: [], invalidIndexes: [] };
+    }
 
     return {
       isValid: false,
       messages: [],
-      invalidIndexes: [],
+      invalidIndexes: [whiteKropkiDot.x1, whiteKropkiDot.x2],
+    };
+  }
+
+  private validateBlackKropkiDot(
+    sudokuString: string,
+    blackKropkiDot: KropkiDot
+  ): ValidatorResult {
+    const value1 = Number(sudokuString.charAt(blackKropkiDot.x1));
+    const value2 = Number(sudokuString.charAt(blackKropkiDot.x2));
+
+    if (
+      value1 * (blackKropkiDot.kropkiValue || 2) === value2 ||
+      value2 * (blackKropkiDot.kropkiValue || 2) === value1
+    ) {
+      return { isValid: true, messages: [], invalidIndexes: [] };
+    }
+
+    return {
+      isValid: false,
+      messages: [],
+      invalidIndexes: [blackKropkiDot.x1, blackKropkiDot.x2],
     };
   }
 }
