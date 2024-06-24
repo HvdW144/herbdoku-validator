1dbaf1fe2e7ae8e767d11b83bfc47da7caefb4b4
HvdW <herbert.vanderwerf@rysst.nl>
2024-06-24 15:49:21 +0200

    added documentation to exported types

--- src/validators/kropki-validator/kropkiDot.interface.ts
+++ src/validators/kropki-validator/kropkiDot.interface.ts
@@ -1,16 +1,16 @@
 export interface KropkiDot {
   /**
-   * The first index of the kropki dot.
+   * The first index of the kropki dot. The first cell that touches the dot.
    */
   x1: number;
   /**
-   * The second index of the kropki dot.
+   * The second index of the kropki dot. The second cell that touches the dot.
    */
   x2: number;
   /**
    * The value of the kropki dot.
-   * For white kropki, the difference between the two values should be this value.
-   * For black kropki, the two values should be in a ratio of this value.
+   * For white kropki, the difference between the two values should be this value, default is 1.
+   * For black kropki, the two values should be in a ratio of this value, default is 2.
    */
   kropkiValue?: number;
   /**
--- src/validators/validatorResultTotal.interface.ts
+++ src/validators/validatorResultTotal.interface.ts
@@ -1,8 +1,14 @@
 export interface ValidatorResultTotal {
+  /**
+   * Contains true when all issued checks are found valid.
+   */
   isValid: boolean;
+  /**
+   * Contains all returned messages, like warnings, errors and further information.
+   */
   messages: string[];
   /**
-   * Indices of the invalid indexes in the input array.
+   * Contains all (unique) **indexes** that are found invalid by one or more of the issued checks.
    */
   invalidIndexes: number[];
 }
