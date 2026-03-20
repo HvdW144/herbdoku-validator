2cc18b5bbfb93d29a19e7bbe87f94ac16e408b17
HvdW <117818214+HvdW144@users.noreply.github.com>
2026-03-20 11:51:36 +0100

    Update validator.type.ts

--- src/types/validator.type.ts
+++ src/types/validator.type.ts
@@ -1,6 +1,6 @@
 import type { ValidatorResult } from "../validators/validatorResult.interface";
 
-export type ValidatorFunction<T = never> = (
+export type ValidatorFunction<T = void> = (
   grid: Uint8Array,
   size: number,
   options?: T,
