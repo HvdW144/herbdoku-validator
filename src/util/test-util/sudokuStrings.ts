f10ab9902dc9d637139d3a0de13e0d76cac7b84e
HvdW <117818214+HvdW144@users.noreply.github.com>
2026-03-22 12:16:26 +0100

    updated test sudoku's

--- src/util/test-util/sudokuStrings.ts
+++ src/util/test-util/sudokuStrings.ts
@@ -1,8 +1,9 @@
 // Contains valid Sudoku strings for testing purposes.
 export const VALID_DEFAULT_SUDOKU_STRING_4X4 = "1234341243212143";
 
-export const VALID_DEFAULT_SUDOKU_STRING_9X9 =
-  "256473891974821536183569427691382754328754169547196283465237918732918645819645372";
+export const VALID_DEFAULT_SUDOKU_UINT8ARRAY_4X4 = new Uint8Array([
+  1, 2, 3, 4, 3, 4, 1, 2, 4, 3, 2, 1, 2, 1, 4, 3,
+]);
 
 export const VALID_DEFAULT_SUDOKU_STRING_2D_4x4 = [
   ["1", "2", "3", "4"],
@@ -10,6 +11,16 @@ export const VALID_DEFAULT_SUDOKU_STRING_2D_4x4 = [
   ["4", "3", "2", "1"],
   ["2", "1", "4", "3"],
 ];
+
+export const VALID_DEFAULT_SUDOKU_STRING_9X9 =
+  "256473891974821536183569427691382754328754169547196283465237918732918645819645372";
+
+export const VALID_DEFAULT_SUDOKU_UINT8ARRAY_9X9 = new Uint8Array([
+  2, 5, 6, 4, 7, 3, 8, 9, 1, 9, 7, 4, 8, 2, 1, 5, 3, 6, 1, 8, 3, 5, 6, 9, 4, 2,
+  7, 6, 9, 1, 3, 8, 2, 7, 5, 4, 3, 2, 8, 7, 5, 4, 1, 6, 9, 5, 4, 1, 9, 6, 2, 8,
+  3, 4, 6, 5, 2, 3, 7, 9, 1, 8, 7, 3, 2, 9, 1, 8, 6, 4, 5, 8, 1, 9, 6, 4, 5, 3,
+  7, 2,
+]);
 
 export const VALID_DEFAULT_SUDOKU_STRING_2D_9x9 = [
   ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
