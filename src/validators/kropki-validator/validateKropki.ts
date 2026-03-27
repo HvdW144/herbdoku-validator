300fe67c36e12dbcf505423c3f67baaad6449338
HvdW <117818214+HvdW144@users.noreply.github.com>
2026-03-27 10:27:34 +0100

    refactor validateKropki

--- src/validators/kropki-validator/validateKropki.ts
+++ src/validators/kropki-validator/validateKropki.ts
@@ -3,6 +3,9 @@ import { ValidatorResult } from "../validatorResult.class";
 import type { IValidatorResult } from "../validatorResult.interface";
 import type { KropkiDot } from "./kropkiDot.interface";
 
+const DEFAULT_WHITE_DIFF = 1;
+const DEFAULT_BLACK_RATIO = 2;
+
 export const validateKropki: ValidatorFunction<KropkiDot[]> = (
   grid: Uint8Array,
   gridSize: number,
@@ -10,67 +13,54 @@ export const validateKropki: ValidatorFunction<KropkiDot[]> = (
 ): IValidatorResult => {
   const finalResult = new ValidatorResult();
 
-  kropkiArray.forEach((kropkiDot) => {
+  for (const kropkiDot of kropkiArray) {
     if (!isValidPosition(kropkiDot.x1, kropkiDot.x2, gridSize)) {
       finalResult.messages.push(
         kropkiOutOfBoundsMessage(kropkiDot.x1, kropkiDot.x2),
       );
-      return;
+      break;
     }
 
-    if (kropkiDot.kropkiType === "white" || kropkiDot.kropkiType === 0) {
-      finalResult.append(validateWhiteKropkiDot(grid, kropkiDot));
-    } else if (kropkiDot.kropkiType === "black" || kropkiDot.kropkiType === 1) {
-      finalResult.append(validateBlackKropkiDot(grid, kropkiDot));
-    } else {
-      throw new Error("Invalid kropki type: " + kropkiDot.kropkiType);
-    }
-  });
+    const validator = getKropkiValidator(kropkiDot.kropkiType);
+    finalResult.append(validator(grid, kropkiDot));
+  }
 
   return finalResult;
 };
 
-const validateWhiteKropkiDot = (
-  grid: Uint8Array,
-  whiteKropkiDot: KropkiDot,
-): IValidatorResult => {
-  const value1 = Number(grid[whiteKropkiDot.x1]);
-  const value2 = Number(grid[whiteKropkiDot.x2]);
-
-  if (
-    value1 - (whiteKropkiDot.kropkiValue || 1) === value2 ||
-    value2 - (whiteKropkiDot.kropkiValue || 1) === value1
-  ) {
-    return { isValid: true, messages: [], invalidIndexes: new Set<number>() };
+const getKropkiValidator = (
+  kropkiType: "white" | "black" | 0 | 1,
+): ((grid: Uint8Array, dot: KropkiDot) => IValidatorResult) => {
+  if (kropkiType === "white" || kropkiType === 0) {
+    return (grid: Uint8Array, dot: KropkiDot) =>
+      validateKropkiDot(grid, dot, whiteKropkiComparison, DEFAULT_WHITE_DIFF);
   }
-
-  return {
-    isValid: false,
-    messages: [],
-    invalidIndexes: new Set<number>([whiteKropkiDot.x1, whiteKropkiDot.x2]),
-  };
+  return (grid: Uint8Array, dot: KropkiDot) =>
+    validateKropkiDot(grid, dot, blackKropkiComparison, DEFAULT_BLACK_RATIO);
 };
 
-const validateBlackKropkiDot = (
+const validateKropkiDot = (
   grid: Uint8Array,
-  blackKropkiDot: KropkiDot,
+  kropkiDot: KropkiDot,
+  compare: (val1: number, val2: number, expectedValue: number) => boolean,
+  defaultValue: number,
 ): IValidatorResult => {
-  const value1 = Number(grid[blackKropkiDot.x1]);
-  const value2 = Number(grid[blackKropkiDot.x2]);
+  const value1 = Number(grid[kropkiDot.x1]);
+  const value2 = Number(grid[kropkiDot.x2]);
+  const expectedValue = kropkiDot.kropkiValue ?? defaultValue;
 
-  if (
-    value1 * (blackKropkiDot.kropkiValue || 2) === value2 ||
-    value2 * (blackKropkiDot.kropkiValue || 2) === value1
-  ) {
-    return { isValid: true, messages: [], invalidIndexes: new Set<number>() };
+  if (compare(value1, value2, expectedValue)) {
+    return new ValidatorResult();
   }
 
-  return {
-    isValid: false,
-    messages: [],
-    invalidIndexes: new Set<number>([blackKropkiDot.x1, blackKropkiDot.x2]),
-  };
+  return new ValidatorResult(false, [], new Set([kropkiDot.x1, kropkiDot.x2]));
 };
+
+const whiteKropkiComparison = (val1: number, val2: number, diff: number) =>
+  val1 - diff === val2 || val2 - diff === val1;
+
+const blackKropkiComparison = (val1: number, val2: number, ratio: number) =>
+  val1 * ratio === val2 || val2 * ratio === val1;
 
 function isValidPosition(x1: number, x2: number, gridSize: number) {
   return x1 >= 0 && x1 < gridSize ** 2 && x2 >= 0 && x2 < gridSize ** 2;
