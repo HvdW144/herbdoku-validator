697fe0ed04bbcb0d13df53c3cddc390807b3f350
HvdW <117818214+HvdW144@users.noreply.github.com>
2026-05-05 14:19:37 +0200

    added validatePalindromes logic

--- src/validators/palindrome-validator/validatePalindromes.ts
+++ src/validators/palindrome-validator/validatePalindromes.ts
@@ -7,10 +7,31 @@ import type { IValidatorResult } from "../validatorResult.interface";
 
 export const validatePalindromes: ValidatorFunction<number[][]> = (
   grid: Uint8Array,
-  gridSize: number,
-  options: number[][],
+  _: number,
+  palindromes: number[][],
 ): IValidatorResult => {
-  throw Error("not implemented");
+  const duplicateIndexes: Set<number> = new Set();
+
+  palindromes.forEach((palindrome) => {
+    const len = palindrome.length;
+
+    // going through only the first half
+    for (let i = 0; i < len / 2; i++) {
+      const startIndex = palindrome[i] as number;
+      const endIndex = palindrome[len - 1 - i] as number;
+
+      if (grid[startIndex] !== grid[endIndex]) {
+        duplicateIndexes.add(startIndex);
+        duplicateIndexes.add(endIndex);
+      }
+    }
+  });
+
+  return {
+    isValid: duplicateIndexes.size === 0,
+    messages: [],
+    invalidIndexes: duplicateIndexes,
+  };
 };
 
 export const validatePalindromesWrapper: ValidatorFunctionWrapper<number[][]> =
